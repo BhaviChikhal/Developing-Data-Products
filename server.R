2d5002914c2f85a5ffcbbcc8c8c3b01a4520cfb7
dafwilli <dafwilli@yahoo.com>
2015-05-06 18:38:42 -0600

    First Commit

---
+++ server.R
@@ -0,0 +1,52 @@
+library(shiny)
+library(ggmap)
+library(dplyr)
+library(lubridate)
+
+# read in the data and the map
+data <- read.csv("train.csv")
+add_data <- read.csv("additional_data.csv")
+map <- readRDS("mapdata_copyright_openstreetmap_contributors.rds")
+
+# make a new variable for month
+data <- mutate(data,Date = ymd(Date))
+data <- mutate(data,dmonth = month(Date))
+data <- mutate(data,dyear = year(Date))
+data <- mutate(data, species=as.factor(Species))
+
+# make a new variable for presence or absence of West Nile Virus
+data <- mutate(data,AbsPres = ifelse(data$WnvPresent==0,"Absence","Presence"))
+
+#select only the data columns needed
+data <- select(data,dmonth,dyear,Longitude,Latitude,species,NumMosquitos,AbsPres)
+
+# bind addition data for missing months (prevents error messages)
+#w <- head(data)
+#write.csv(w,file="additional_data.csv",row.names=FALSE)
+data <- rbind(data,add_data)
+
+#Define server logic required to generate and plot a mosquito heat map of chicago
+shinyServer(function(input, output) {
+        
+        dataInput <- reactive({
+                dataf <- filter(data,dmonth==input$dMonth,dyear==input$dYear,species == input$species)
+        })
+                
+        # plot map 
+        output$MapPlot <- renderPlot({
+                df <- dataInput()
+                # generate map of Chicago
+                ggmap(map) +
+                        geom_point(data=df,aes(x=Longitude,y=Latitude,colour=AbsPres,size=NumMosquitos),alpha=0.5)+
+                        facet_grid(.~AbsPres)
+        })
+        
+        # render a summary data
+        output$summary <- renderPrint({
+                dataset <- dataInput()
+                mean(dataset$WnvPresent)
+        })
+        
+        
+        
+})
---
+++ ui.R
@@ -0,0 +1,41 @@
+library(shiny)
+
+# Define UI with mosquito heat map or Chicago
+
+shinyUI(pageWithSidebar(
+        
+        #Application Title
+        headerPanel("Mosquito Heat Map"),
+        
+        # Sidebar with a slider input for month
+        sidebarPanel(
+                sliderInput("dMonth", 
+                            "Month of observation:", 
+                            min = 5,
+                            max = 10, 
+                            value = 5),
+                
+                sliderInput("dYear", 
+                            "Year of observation:", 
+                            min = 2007,
+                            max = 2013, 
+                            value = 2007,
+                            step = 2),
+                selectInput("species",
+                            "Mosquito Species",
+                            choices = c("CULEX RESTUANS","CULEX PIPIENS","CULEX PIPIENS/RESTUANS",
+                                        "CULEX TERRITANS","CULEX SALINARIUS","CULEX TARSALIS")
+                )
+        ),
+        
+        # Summary of dataframe
+        mainPanel(
+        # Show a plot heatmap
+                plotOutput("MapPlot"),
+
+        # Show the mean of virus carrying mosquitos
+                verbatimTextOutput("summary") 
+        )
+))
+
+        
